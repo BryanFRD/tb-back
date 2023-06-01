0c6f334c27c02fb3d3c4cc880d49c2ab58d5f498
Bryan <bryanferrando59@gmail.com>
2023-06-01 19:10:36 +0200

    Add DTO

---
+++ src/DTO/ServiceResponseDTO.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DTO;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class ServiceResponseDTO {
+  
+  private mixed $data;
+  private int $statusCode;
+  
+  public function __construct(mixed $data, int $statusCode = Response::HTTP_OK){
+    $this->data = $data;
+    $this->statusCode = $statusCode;
+  }
+  
+  public function getData(): mixed {
+    return $this->data;
+  }
+  
+  public function getStatusCode(): int {
+    return $this->statusCode;
+  }
+  
+}
